e5a20ab379d9888d32d11fffb9dfcfebebf9958f
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-14 20:43:02 -0600

    angular working and script tags placed

---
+++ app/app.js
@@ -0,0 +1,13 @@
+var app = angular.module("Loc-Inf",[ 'ngRoute']);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
