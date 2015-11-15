e16f0bf40ad083eecf0b895744223c5fcfd1f204
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-14 23:12:21 -0600

    fixed align issue

--- app/controllers/ZTCCtrl.js
+++ app/controllers/ZTCCtrl.js
@@ -1,17 +1,18 @@
 app.controller("ZTCCtrl", 
   ["$scope", "$http",
   function($scope, $http) {
-    $scope.input;
+    $scope.input = "";
 
     // runAjax();
 
     $scope.getLocation = function(){
+      console.log("clicked");
       // $http.get("http://api.zippopotam.us/us/ks/spring hill/18950")
       $http.get("http://api.zippopotam.us/us/" + $scope.input)
       .then(function(data){
         console.log(data);
         $scope.ajax = data;
-      })
-    }
+      });
+    };
   }
 ]);
