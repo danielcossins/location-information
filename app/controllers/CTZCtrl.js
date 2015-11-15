6751f7399b516c97af4fdcb88bfca929b85200f8
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-15 16:36:52 -0600

    getting places displaying

--- app/controllers/CTZCtrl.js
+++ app/controllers/CTZCtrl.js
@@ -1,8 +1,9 @@
 app.controller("CTZCtrl", 
   ["$scope", "$http",
   function($scope, $http) {
-    $scope.state = "";
+    $scope.state = "AL";
     $scope.city = "";
+    $scope.places = [];
 
     // runAjax();
 
@@ -13,7 +14,18 @@ app.controller("CTZCtrl",
       $http.get("http://api.zippopotam.us/us/" + $scope.state + "/" + $scope.city)
       .then(function(data){
         console.log(data);
-        $scope.ajax = data;
+        var obj = {
+          country: data.data.country,
+          countryAbbr: data.data['country abbreviation'],
+          city: data.data['place name'],
+          state: data.data.state,
+          stateAbbr: data.data['state abbreviation']
+        };
+        for(var i=0; i<data.data.places.length; i++){
+          $scope.places.push(data.data.places[i]);
+        }
+        console.log(obj);
+        $scope.ajax = obj;
       });
     };
   }
