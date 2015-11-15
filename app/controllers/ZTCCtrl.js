41cae0767ad945759ea6ac221e1a176d645525c1
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-14 23:35:31 -0600

    hard coded working countries

--- app/controllers/ZTCCtrl.js
+++ app/controllers/ZTCCtrl.js
@@ -1,14 +1,15 @@
 app.controller("ZTCCtrl", 
   ["$scope", "$http",
   function($scope, $http) {
+    $scope.country = "";
     $scope.input = "";
 
     // runAjax();
 
     $scope.getLocation = function(){
-      console.log("clicked");
+      console.log($scope.country);
       // $http.get("http://api.zippopotam.us/us/ks/spring hill/18950")
-      $http.get("http://api.zippopotam.us/us/" + $scope.input)
+      $http.get("http://api.zippopotam.us/" + $scope.country + "/" + $scope.input)
       .then(function(data){
         console.log(data);
         $scope.ajax = data;
