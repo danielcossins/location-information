4689abb06a0e672f73fc710075a2ba46052bf151
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-16 21:04:29 -0600

    styled select box

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -27,17 +27,5 @@ app.controller("MainCtrl",
         angular.element('#ztcB').css("color", "grey");
       }
     });
-    // $scope.input;
-
-    // // runAjax();
-
-    // $scope.getLocation = function(){
-    //   $http.get("http://api.zippopotam.us/us/ks/spring hill/18950")
-    //   // $http.get("http://api.zippopotam.us/us/" + $scope.input)
-    //   .then(function(data){
-    //     console.log(data);
-    //     $scope.ajax = data;
-    //   })
-    // }
   }
 ]);
