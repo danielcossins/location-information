f1ebf8750781ade9a74b8b854773816205f1527d
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-16 19:31:32 -0600

    added background image and more styling

--- app/controllers/ZTCCtrl.js
+++ app/controllers/ZTCCtrl.js
@@ -14,13 +14,13 @@ app.controller("ZTCCtrl",
         console.log(data);
         var obj = {
           country: data.data.country,
-          countryAbrev: data.data['country abbreviation'],
+          countryAbbr: data.data['country abbreviation'],
           zipCode: data.data['post code'],
           lat: data.data.places[0].latitude,
           lon: data.data.places[0].longitude,
           city: data.data.places[0]['place name'],
           state: data.data.places[0].state,
-          stateAbrev: data.data.places[0]['state abbreviation']
+          stateAbbr: data.data.places[0]['state abbreviation']
         };
         console.log(obj);
         $scope.ajax = obj;
